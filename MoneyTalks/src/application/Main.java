df0b884f280bcff4d5e21b4ce2e6285d4ac9269b
eduardorcury <eduardo@EduardoHP>
2020-02-13 23:29:32 -0400

    Data Picker style completed

--- MoneyTalks/src/application/Main.java
+++ MoneyTalks/src/application/Main.java
@@ -3,8 +3,11 @@ package application;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TreeView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -21,6 +24,18 @@ public class Main extends Application {
 	}
 
 	public void start(Stage primaryStage) throws Exception {
+		
+		ColorPicker colorPicker = new ColorPicker();
+		colorPicker.getStyleClass().add("split-button");
+		
+		DatePicker datePicker1 = new DatePicker();
+		DatePicker datePicker2 = new DatePicker();
+		datePicker1.getStylesheets().add("DatePicker.css");
+		
+		datePicker1.setPromptText("Enter item date");
+		
+		VBox dateBox = new VBox();
+		dateBox.getChildren().addAll(datePicker1, datePicker2);
 
 		window = primaryStage;
 		window.setTitle("MoneyTalks");
@@ -28,7 +43,8 @@ public class Main extends Application {
 		
 		BorderPane layout = new BorderPane();
 		layout.setTop(menu.menuItems());
-		// layout.setLeft(monthTree);
+		layout.setCenter(colorPicker);
+		layout.setLeft(dateBox);
 		layout.setRight(dataLayout.newData());
 		scene = new Scene(layout, 400, 300);
 		window.setScene(scene);
